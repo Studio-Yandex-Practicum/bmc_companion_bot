349514049de8a4f6a879833555e7c94c7b1b48e0
thalq <95490979+thalq@users.noreply.github.com>
2022-12-20 22:38:10 +0300

    feat: элементы меню вынесены в отдельный класс

--- bot/src/ui/main_menu.py
+++ bot/src/ui/main_menu.py
@@ -4,8 +4,33 @@ from telegram.ext import CallbackContext
 from . import constants as const
 
 
+class MenuElement:
+    def __init__(self, name, result, answers):
+        self.name = name
+        self.result = result
+        self.answers = answers
+
+    def __call__(self, answer_name: str) -> str:
+        return self.answers[answer_name]
+
+
+class MenuNames:
+    """Текстовое описание для элементов меню."""
+
+    start_menu = MenuElement(
+        name=const.NAME,
+        result=const.GREETING_MESSAGE,
+        answers=const.ANSWERS,
+    )
+
+
 def main_menu(update: Update, context: CallbackContext):
-    buttons = ReplyKeyboardMarkup([["Тесты"], ["Запись к психологу"]], resize_keyboard=True)
+    buttons = ReplyKeyboardMarkup(
+        [[MenuNames.start_menu("to_test")], [MenuNames.start_menu("to_meeting")]],
+        resize_keyboard=True,
+    )
     context.bot.send_message(
-        chat_id=update.effective_chat.id, text=const.GREETING_MESSAGE, reply_markup=buttons
+        chat_id=update.effective_chat.id,
+        text=MenuNames.start_menu.result.format(update.message.chat.first_name),
+        reply_markup=buttons,
     )
