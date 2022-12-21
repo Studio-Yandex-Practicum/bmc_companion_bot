bfb372a76f75dad4acfd7708f3670b657793f533
thalq <95490979+thalq@users.noreply.github.com>
2022-12-21 12:22:54 +0300

    feat: удален файл констант

--- bot/src/ui/core.py
+++ bot/src/ui/core.py
@@ -1,6 +1,3 @@
-from ui import constants as const
-
-
 class MenuElement:
     def __init__(self, name: str, result: str, answers: dict[str, str]) -> None:
         self.name = name
@@ -15,7 +12,8 @@ class MenuNames:
     """Текстовое описание для элементов меню."""
 
     start_menu = MenuElement(
-        name=const.NAME,
-        result=const.GREETING_MESSAGE,
-        answers=const.ANSWERS,
+        name="Меню",
+        result="""Привет, {}! Мы приветствуем Ваше решение разобраться в себе.
+                В нашем проекте есть настоящие профессионалы, которые помогут Вам сделать это.""",
+        answers={"to_test": "Тесты", "to_meeting": "Записаться к психологу"},
     )
