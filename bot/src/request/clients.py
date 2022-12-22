d2712481ffd42441501c25a0478cb0633870a542
Alexander Drakon <drakon.a.v@gmail.com>
2022-12-22 18:09:30 +0300

    fix: правки по ревью PR

--- bot/src/request/clients.py
+++ bot/src/request/clients.py
@@ -1,8 +1,13 @@
+from typing import Dict, List, Optional, Type, TypeVar, Union
+
 import httpx
+from core.constants import Endpoint, HTTPMethod
 from core.settings import settings
-from pydantic import ValidationError
+from pydantic import BaseModel, ValidationError
 
 WEB_API_URL = f"{settings.APP_HOST}:{settings.APP_PORT}"
+
+ModelType = TypeVar("ModelType", bound=BaseModel)
 
 
 class APIClientException(Exception):
@@ -10,12 +15,18 @@ class APIClientException(Exception):
 
 
 class APIClient:
-    def __init__(self, endpoint, model, manymodel):
+    def __init__(
+        self, endpoint: Endpoint, model: Type[ModelType], manymodel: Type[ModelType]
+    ) -> None:
         self.model = model
         self.manymodel = manymodel
         self.base_url = f"http://{WEB_API_URL}/{endpoint}"
 
-    def __assert_response_ok(self, response):
+    def _assert_response_ok(self, response: httpx.Response) -> httpx.Response:
+        """
+        Проверяет, что статус-код соответствует значению OK, и бросает исключение,
+        если это не так.
+        """
         try:
             response.raise_for_status()
         except httpx.HTTPStatusError as e:
@@ -24,28 +35,47 @@ class APIClient:
             )
         return response
 
-    def __process_response(self, response, pydantic_model):
-        response = self.__assert_response_ok(response)
+    def _process_response(
+        self, response: httpx.Response, pydantic_model: Type[ModelType]
+    ) -> ModelType:
+        """
+        Принимает HTTP-запрос, проверяет, что статус-код соответствует OK, и преобразует его
+        в объект модели pydantic.
+        """
+        response = self._assert_response_ok(response)
         try:
             obj = pydantic_model.parse_obj(response.json())
         except ValidationError as e:
             raise APIClientException(f"Unexpected format of API response: {e.json()}")
         return obj
 
-    def __safe_request(self, method, url, json=None, params=None):
+    def _safe_request(
+        self,
+        method: str,
+        url: str,
+        json: Optional[str] = None,
+        params: Optional[Dict[str, str]] = None,
+    ) -> httpx.Response:
+        """Метод, реализующий HTTP-запрос и перехват возникающих при этом исключений."""
         try:
             response = httpx.request(method=method, url=url, json=json, params=params)
         except httpx.RequestError as e:
             raise APIClientException(f"Error while requesting {e.request.url}")
         return response
 
-    def __obj_url(self, id):
+    def _obj_url(self, id):
+        """Конструирует URL ресурса, используя переданный id."""
         return f"{self.base_url}/{id}"
 
-    def get(self, id=None, offset=None, limit=None):
+    def get(
+        self,
+        id: Optional[Union[int, str]] = None,
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+    ) -> Union[ModelType, List[ModelType]]:
         if id is not None:
-            response = self.__safe_request("GET", self.__obj_url(id))
-            obj = self.__process_response(response, self.model)
+            response = self._safe_request(HTTPMethod.GET, self._obj_url(id))
+            obj = self._process_response(response, self.model)
             return obj
         params = {}
         if limit:
@@ -53,21 +83,21 @@ class APIClient:
         if offset:
             params["offset"] = offset
         print(params)
-        response = self.__safe_request("GET", self.base_url, params=params)
-        objs = self.__process_response(response, self.manymodel)
+        response = self._safe_request(HTTPMethod.GET, self.base_url, params=params)
+        objs = self._process_response(response, self.manymodel)
         return objs
 
-    def create(self, obj):
-        response = self.__safe_request("POST", self.base_url, json=obj.json())
-        obj = self.__process_response(response, self.model)
+    def create(self, obj: ModelType) -> ModelType:
+        response = self._safe_request(HTTPMethod.POST, self.base_url, json=obj.json())
+        obj = self._process_response(response, self.model)
         return obj
 
-    def update(self, id, obj):
-        response = self.__safe_request("PATCH", self.__obj_url(id), json=obj.json())
-        obj = self.__process_response(response, self.model)
+    def update(self, id: Union[int, str], obj: ModelType) -> ModelType:
+        response = self._safe_request(HTTPMethod.PATCH, self._obj_url(id), json=obj.json())
+        obj = self._process_response(response, self.model)
         return obj
 
-    def delete(self, id):
-        response = self.__safe_request("DELETE", self.__obj_url(id))
-        obj = self.__process_response(response, self.model)
+    def delete(self, id: Union[int, str]) -> ModelType:
+        response = self._safe_request(HTTPMethod.DELETE, self._obj_url(id))
+        obj = self._process_response(response, self.model)
         return obj
