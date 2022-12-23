931813606298dd74ae5edf1f9b956bdf96cbce0f
thalq <95490979+thalq@users.noreply.github.com>
2022-12-23 13:53:00 +0300

    feat: добавлен роутер questions

--- webapi/src/app/__init__.py
+++ webapi/src/app/__init__.py
@@ -1,6 +1,7 @@
 from app.core.settings import settings
 from app.db.pg import db, migrate
 from flask import Flask
+from flask_restful import Api
 
 
 def create_app():
@@ -9,10 +10,13 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    api = Api(app)
 
     from app.api import bp as api_bp
+    from app.api.v1.questions import register_router as router
 
     app.register_blueprint(api_bp)
+    router(api)
 
     return app
 
