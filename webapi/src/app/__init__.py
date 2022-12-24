dfc30b35cf97b983ff8071bb1b51dcd2d1adc086
Alexey T <88614947+learies@users.noreply.github.com>
2022-12-24 15:43:32 +0300

    Revert "Feat/question type"

--- webapi/src/app/__init__.py
+++ webapi/src/app/__init__.py
@@ -1,7 +1,6 @@
 from app.core.settings import settings
 from app.db.pg import db, migrate
 from flask import Flask
-from flask_restful import Api
 
 
 def create_app():
@@ -10,13 +9,10 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
-    api = Api(app)
 
     from app.api import bp as api_bp
-    from app.api.v1.questions import register_router as router
 
     app.register_blueprint(api_bp)
-    router(api)
 
     return app
 
--- webapi/src/app/api/v1/questions/__init__.py
+++
@@ -1,1 +0,0 @@
-from app.api.v1.questions.routers import register_router
