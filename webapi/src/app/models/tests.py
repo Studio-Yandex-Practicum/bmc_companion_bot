2dabfbb8f8720e3194e0fe28b39f6118eacc5219
thalq <95490979+thalq@users.noreply.github.com>
2022-12-27 20:49:39 +0300

    fix: возвращены методы to_dict и from_dict

--- webapi/src/app/models/tests.py
+++ webapi/src/app/models/tests.py
@@ -12,3 +12,16 @@ class QuestionType(BaseModel):
         Text, comment="Возможная валидация ответа на вопрос с помощью regexp"
     )
     deleted_at = Column(DateTime, comment="Время удаления")
+
+    def to_dict(self):
+        return dict(
+            name=self.name,
+            validation_regexp=self.validation_regexp,
+            created_at=self.created_at,
+            updated_at=self.updated_at,
+            deleted_at=self.deleted_at,
+        )
+
+    def from_dict(self, data):
+        for key, item in data.items():
+            setattr(self, key, item)
