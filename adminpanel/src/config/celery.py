2cbeb77217178e95154bd2d3a06e337e6525fee1
bmu0 <bmu0@yandex.ru>
2023-02-05 20:34:15 +0300

    fix: cleanup

--- adminpanel/src/config/celery.py
+++ adminpanel/src/config/celery.py
@@ -1,14 +1,8 @@
 import os
-from pathlib import Path
 
 from celery import Celery
-from config.settings.components import config
-
-ROOT_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-BOT_TOKEN = config.BOT_TOKEN
 
 app = Celery("config.celery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
