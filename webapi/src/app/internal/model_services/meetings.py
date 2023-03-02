6d3481d2ff831384b0b0a9a9670a5e8f5075f654
Ali Bogatyrev <marksheyder06@gmail.com>
2023-03-02 20:10:58 +0300

    feat: add Meeting feedback sevice

--- webapi/src/app/internal/model_services/meetings.py
+++ webapi/src/app/internal/model_services/meetings.py
@@ -1,6 +1,6 @@
 from app.db.pg import db
 from app.internal.model_services import BaseModelService
-from app.models import Meeting, MeetingType
+from app.models import Meeting, MeetingFeedback, MeetingType
 from app.schemas.meetings import MeetingTypeList
 
 
@@ -16,3 +16,8 @@ class MeetingTypeModelService(BaseModelService):
     def check_exists_object(self, name: str) -> None | MeetingTypeList:
         """Проверяет наличее объекта по name."""
         return db.session.query(self.model).where(self.model.name == name).first()
+
+
+class MeetingFeedbackService(BaseModelService):
+    def __init__(self, model):
+        super().__init__(MeetingFeedback)
