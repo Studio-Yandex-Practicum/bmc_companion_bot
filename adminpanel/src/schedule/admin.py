af8a9e1b0bf84a667228012450b95d83203a2db5
bmu0 <bmu0@yandex.ru>
2023-03-09 01:06:35 +0300

    feat: feedback export to xlsx

--- adminpanel/src/schedule/admin.py
+++ adminpanel/src/schedule/admin.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
+import openpyxl
 from django.contrib import admin
+from django.http import HttpResponse
 
 from . import models
 
@@ -32,12 +36,69 @@ class MeetingAdmin(admin.ModelAdmin):
     list_filter = ("psychologist", "format")
 
 
+@admin.action(description="Загрузить как файл *.xlsx")
+def export_table_to_excel(modeladmin, request, queryset):
+    data = (
+        models.MeetingFeedback.objects.all()
+        .order_by("meeting__timeslot__date_start")
+        .values(
+            "meeting__timeslot__date_start",
+            "meeting__psychologist__last_name",
+            "meeting__psychologist__first_name",
+            "meeting__psychologist__telegram_login",
+            "user__telegram_login",
+            "text",
+            "score",
+        )
+    )
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.append(
+        [
+            "Дата",
+            "Время",
+            "Психолог",
+            "Телеграм психолога",
+            "Телеграм пациента",
+            "Отзыв",
+            "Score",
+        ]
+    )
+    for row in data:
+        ws.append(
+            [
+                row["meeting__timeslot__date_start"].strftime("%d.%m.%Y"),
+                row["meeting__timeslot__date_start"].strftime("%H:%M"),
+                row["meeting__psychologist__last_name"]
+                + " "
+                + row["meeting__psychologist__first_name"],
+                row["meeting__psychologist__telegram_login"],
+                row["user__telegram_login"],
+                row["text"],
+                row["score"],
+            ]
+        )
+    response = HttpResponse(
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+    response["Content-Disposition"] = f"attachment; filename=feedbacks-{datetime.now()}.xlsx"
+    wb.save(response)
+
+    return response
+
+
 @admin.register(models.MeetingFeedback)
 class MeetingFeedbackAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "meeting", "score")
+    list_display = (
+        "id",
+        "user",
+        "meeting",
+        "score",
+    )
     list_select_related = ("meeting", "user")
     search_fields = (
         "user__first_name",
         "meeting__id",
     )
     list_filter = ("user", "meeting", "score")
+    actions = [export_table_to_excel]
