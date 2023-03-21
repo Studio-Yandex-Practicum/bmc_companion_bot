df9fcd0b9b7a999a0c2d4b0a07c3485a53fc40e9
ADesBiysk <adesbiysk@yandex.ru>
2023-03-22 02:44:17 +0700

    feat: add migrations

--- adminpanel/src/profiles/admin.py
+++ adminpanel/src/profiles/admin.py
@@ -1,13 +1,14 @@
-from datetime import datetime
+# from datetime import datetime
 
-import openpyxl
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.http import HttpResponse
-from questioning.models import TestCompleted
-from schedule.models import MeetingFeedback
 
+# from questioning.models import TestCompleted
+# from schedule.models import MeetingFeedback
 from . import models
+
+# from django.http import HttpResponse
+# from openpyxl import Workbook
 
 EXPORT_FIELDS = ("id", "first_name", "last_name", "phone", "telegram_login", "age")
 TEST_EXPORT_FIELDS = ("Название теста", "Баллы")
@@ -22,61 +23,61 @@ FEEDBACK_EXPORT_FIELDS = (
 )
 
 
-@admin.action(description="Загрузить как файл *.xlsx")
-def export_table_to_excel(modeladmin, request, queryset):
-    wb = openpyxl.Workbook()
-    ws = wb.active
-    qs_fields = queryset.model._meta.fields
-    user_fields = [f.name for f in qs_fields if f.name in EXPORT_FIELDS]
-    user_fields_ru = [f.verbose_name for f in qs_fields if f.name in EXPORT_FIELDS]
-    test_results = TestCompleted.objects.all()
-    feedbacks = MeetingFeedback.objects.all()
-    row = 1
-    for user in queryset.values_list(*user_fields):
-        user_test_results = test_results.filter(profile=int(user[0]))
-        user_feedbacks = feedbacks.filter(user=int(user[0]))
-        for col_num, field_name in enumerate(user_fields_ru):
-            if col_num > 0:
-                ws.cell(row=row, column=col_num).value = str(field_name).capitalize()
-        row += 1
-        for col_num, field_value in enumerate(user):
-            if col_num > 0:
-                ws.cell(row=row, column=col_num).value = str(field_value)
-        row += 2
-        if user_test_results:
-            for col_num, field_name in enumerate(TEST_EXPORT_FIELDS):
-                ws.cell(row=row, column=col_num + 1).value = str(field_name)
-            row += 1
-            for row_num, test in enumerate(user_test_results):
-                ws.cell(row=row, column=1).value = str(test.test.name)
-                ws.cell(row=row, column=2).value = str(test.value)
-                row += 1
-            row += 1
-        if user_feedbacks:
-            for col_num, field_name in enumerate(FEEDBACK_EXPORT_FIELDS):
-                ws.cell(row=row, column=col_num + 1).value = str(field_name)
-            row += 1
-            for row_num, feedback in enumerate(user_feedbacks):
-                ws.cell(row=row, column=1).value = str(
-                    feedback.meeting.date_start.strftime("%d.%m.%Y")
-                )
-                ws.cell(row=row, column=2).value = str(
-                    feedback.meeting.date_start.strftime("%H:%M")
-                )
-                ws.cell(row=row, column=3).value = str(feedback.meeting.psychologist.last_name)
-                ws.cell(row=row, column=4).value = str(feedback.meeting.psychologist.first_name)
-                ws.cell(row=row, column=5).value = str(feedback.meeting.psychologist.telegram_login)
-                ws.cell(row=row, column=6).value = str(feedback.text)
-                ws.cell(row=row, column=7).value = str(feedback.score)
-                row += 1
-            row += 1
-        row += 1
-    response = HttpResponse(
-        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    )
-    response["Content-Disposition"] = f"attachment; filename=profile-{datetime.now()}.xlsx"
-    wb.save(response)
-    return response
+# @admin.action(description="Загрузить как файл *.xlsx")
+# def export_table_to_excel(modeladmin, request, queryset):
+#     wb = Workbook()
+#     ws = wb.active
+#     qs_fields = queryset.model._meta.fields
+#     user_fields = [f.name for f in qs_fields if f.name in EXPORT_FIELDS]
+#     user_fields_ru = [f.verbose_name for f in qs_fields if f.name in EXPORT_FIELDS]
+#     test_results = TestCompleted.objects.all()
+#     feedbacks = MeetingFeedback.objects.all()
+#     row = 1
+#     for user in queryset.values_list(*user_fields):
+#         user_test_results = test_results.filter(profile=int(user[0]))
+#         user_feedbacks = feedbacks.filter(user=int(user[0]))
+#         for col_num, field_name in enumerate(user_fields_ru):
+#             if col_num > 0:
+#                 ws.cell(row=row, column=col_num).value = str(field_name).capitalize()
+#         row += 1
+#         for col_num, field_value in enumerate(user):
+#             if col_num > 0:
+#                 ws.cell(row=row, column=col_num).value = str(field_value)
+#         row += 2
+#         if user_test_results:
+#             for col_num, field_name in enumerate(TEST_EXPORT_FIELDS):
+#                 ws.cell(row=row, column=col_num + 1).value = str(field_name)
+#             row += 1
+#             for row_num, test in enumerate(user_test_results):
+#                 ws.cell(row=row, column=1).value = str(test.test.name)
+#                 ws.cell(row=row, column=2).value = str(test.value)
+#                 row += 1
+#             row += 1
+#         if user_feedbacks:
+#             for col_num, field_name in enumerate(FEEDBACK_EXPORT_FIELDS):
+#                 ws.cell(row=row, column=col_num + 1).value = str(field_name)
+#             row += 1
+#             for row_num, feedback in enumerate(user_feedbacks):
+#                 ws.cell(row=row, column=1).value = str(
+#                     feedback.meeting.date_start.strftime("%d.%m.%Y")
+#                 )
+#                 ws.cell(row=row, column=2).value = str(
+#                     feedback.meeting.date_start.strftime("%H:%M")
+#                 )
+#                 ws.cell(row=row, column=3).value = str(feedback.meeting.psychologist.last_name)
+#                 ws.cell(row=row, column=4).value = str(feedback.meeting.psychologist.first_name)
+#               ws.cell(row=row, column=5).value = str(feedback.meeting.psychologist.telegram_login)
+#                 ws.cell(row=row, column=6).value = str(feedback.text)
+#                 ws.cell(row=row, column=7).value = str(feedback.score)
+#                 row += 1
+#             row += 1
+#         row += 1
+#     response = HttpResponse(
+#         content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+#     )
+#     response["Content-Disposition"] = f"attachment; filename=profile-{datetime.now()}.xlsx"
+#     wb.save(response)
+#     return response
 
 
 @admin.register(models.Profile)
@@ -92,7 +93,7 @@ class ProfileAdmin(UserAdmin):
         "telegram_id",
         "chat_id",
     )
-    actions = [export_table_to_excel]
+    # actions = [export_table_to_excel]
     list_filter = ("is_staff", "is_active")
     fieldsets = (
         (
--- adminpanel/src/schedule/admin.py
+++ adminpanel/src/schedule/admin.py
@@ -1,10 +1,11 @@
-from datetime import datetime
+# from datetime import datetime
 
-import openpyxl
 from django.contrib import admin
-from django.http import HttpResponse
 
 from . import models
+
+# from django.http import HttpResponse
+# from openpyxl import Workbook
 
 
 @admin.register(models.TimeSlot)
@@ -36,55 +37,55 @@ class MeetingAdmin(admin.ModelAdmin):
     list_filter = ("psychologist", "format")
 
 
-@admin.action(description="Загрузить как файл *.xlsx")
-def export_table_to_excel(modeladmin, request, queryset):
-    data = (
-        models.MeetingFeedback.objects.all()
-        .order_by("meeting__timeslot__date_start")
-        .values(
-            "meeting__timeslot__date_start",
-            "meeting__psychologist__last_name",
-            "meeting__psychologist__first_name",
-            "meeting__psychologist__telegram_login",
-            "user__telegram_login",
-            "text",
-            "score",
-        )
-    )
-    wb = openpyxl.Workbook()
-    ws = wb.active
-    ws.append(
-        [
-            "Дата",
-            "Время",
-            "Психолог",
-            "Телеграм психолога",
-            "Телеграм пациента",
-            "Отзыв",
-            "Оценка",
-        ]
-    )
-    for row in reversed(data):
-        ws.append(
-            [
-                row["meeting__timeslot__date_start"].strftime("%d.%m.%Y"),
-                row["meeting__timeslot__date_start"].strftime("%H:%M"),
-                row["meeting__psychologist__last_name"]
-                + " "
-                + row["meeting__psychologist__first_name"],
-                row["meeting__psychologist__telegram_login"],
-                row["user__telegram_login"],
-                row["text"],
-                row["score"],
-            ]
-        )
-    response = HttpResponse(
-        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    )
-    response["Content-Disposition"] = f"attachment; filename=feedbacks-{datetime.now()}.xlsx"
-    wb.save(response)
-
-    return response
+# @admin.action(description="Загрузить как файл *.xlsx")
+# def export_table_to_excel(modeladmin, request, queryset):
+#     data = (
+#         models.MeetingFeedback.objects.all()
+#         .order_by("meeting__timeslot__date_start")
+#         .values(
+#             "meeting__timeslot__date_start",
+#             "meeting__psychologist__last_name",
+#             "meeting__psychologist__first_name",
+#             "meeting__psychologist__telegram_login",
+#             "user__telegram_login",
+#             "text",
+#             "score",
+#         )
+#     )
+#     wb = Workbook()
+#     ws = wb.active
+#     ws.append(
+#         [
+#             "Дата",
+#             "Время",
+#             "Психолог",
+#             "Телеграм психолога",
+#             "Телеграм пациента",
+#             "Отзыв",
+#             "Оценка",
+#         ]
+#     )
+#     for row in reversed(data):
+#         ws.append(
+#             [
+#                 row["meeting__timeslot__date_start"].strftime("%d.%m.%Y"),
+#                 row["meeting__timeslot__date_start"].strftime("%H:%M"),
+#                 row["meeting__psychologist__last_name"]
+#                 + " "
+#                 + row["meeting__psychologist__first_name"],
+#                 row["meeting__psychologist__telegram_login"],
+#                 row["user__telegram_login"],
+#                 row["text"],
+#                 row["score"],
+#             ]
+#         )
+#     response = HttpResponse(
+#         content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+#     )
+#     response["Content-Disposition"] = f"attachment; filename=feedbacks-{datetime.now()}.xlsx"
+#     wb.save(response)
+#
+#     return response
 
 
 @admin.register(models.MeetingFeedback)
@@ -101,4 +102,4 @@ class MeetingFeedbackAdmin(admin.ModelAdmin):
         "meeting__id",
     )
     list_filter = ("user", "meeting", "score")
-    actions = [export_table_to_excel]
+    # actions = [export_table_to_excel]
