f7884d41b911cf125d9d6e7103940c41eeeb2805
bmu0 <bmu0@yandex.ru>
2023-03-23 04:08:38 +0300

    fix: add-migrations

--- adminpanel/src/profiles/migrations/0001_initial.py
+++ adminpanel/src/profiles/migrations/0001_initial.py
@@ -146,22 +146,4 @@ class Migration(migrations.Migration):
                 ("objects", django.contrib.auth.models.UserManager()),
             ],
         ),
-        migrations.CreateModel(
-            name="AllUsersNotification",
-            fields=[
-                (
-                    "periodictask_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="django_celery_beat.periodictask",
-                    ),
-                ),
-                ("text", models.TextField()),
-            ],
-            bases=("django_celery_beat.periodictask",),
-        ),
     ]
