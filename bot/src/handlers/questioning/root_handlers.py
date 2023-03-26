f3cadee3121290778790469a4205b2c209f5898a
bmu0 <bmu0@yandex.ru>
2023-03-26 18:19:48 +0300

    fix: tests

--- bot/src/handlers/questioning/root_handlers.py
+++ bot/src/handlers/questioning/root_handlers.py
@@ -41,7 +41,6 @@ async def test_questioning_section(update: Update, context: ContextTypes.DEFAULT
     else:
         text = "Вы уже прошли все доступные тесты."
         await update.message.reply_text(text)
-        await back_to_start_menu(update, context)
         return BotState.STOPPING
     buttons = []
     context_manager.set_tests(context, {})
