7375a194c1e90073b209a71bc322d28eb6db8f3b
bmu0 <bmu0@yandex.ru>
2023-03-26 15:07:38 +0300

    fix: tests

--- bot/src/handlers/questioning/questioning.py
+++ bot/src/handlers/questioning/questioning.py
@@ -1,10 +1,6 @@
 from core.constants import BotState
 from decorators import at
-from handlers.questioning.root_handlers import (
-    api_client,
-    back_to_start_menu,
-    test_questioning_section,
-)
+from handlers.questioning.root_handlers import api_client, test_questioning_section
 from request.exceptions import NoNextQuestion
 from schemas.requests import (
     UceTestRequest,
@@ -63,8 +59,8 @@ async def submit_answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     answer_text = update.message.text
     answers = context_manager.get_answers(context)
     if not answers:
-        await back_to_start_menu(update, context)
-        return BotState.STOPPING
+        bot_state = await show_result(update, context)
+        return bot_state
     if answer_text not in context_manager.get_answers(context):
         await update.message.reply_text(
             "Выберите ответ из предложенных вариантов",
